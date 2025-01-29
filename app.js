e31b5ff4dbe88f16a0175a02ef19c8fa1b35c780
omjain4 <119519103+omjain4@users.noreply.github.com>
2025-01-29 23:23:24 +0530

    dfhse

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-//jshint esversion:6
+// jshint esversion: 6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _=require("lodash");
+const _ = require("lodash");
 const mongoose = require("mongoose");
+const serverless = require("serverless-http");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam...";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque...";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien...";
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect("mongodb+srv://omjain4:omjain4@cluster0.4zclh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useUnifiedTopology: true, useNewUrlParser: true });
 
 // Post Schema
 const postSchema = new mongoose.Schema({
@@ -44,17 +45,17 @@ app.get("/", async function(req, res) {
      console.error(err);
      res.status(500).send("Error retrieving posts.");
    }
- });
-
-app.get("/about",function(req,res){
-  res.render("about",{aboutContent:aboutContent});
 });
 
-app.get("/contact",function(req,res){
-  res.render("contact",{contactContent:contactContent});
+app.get("/about", function(req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/compose",function(req,res){
+app.get("/contact", function(req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
@@ -71,8 +72,7 @@ app.post("/compose", async function(req, res) {
      console.error(err);
      res.status(500).send("Error saving post.");
    }
- });
- 
+});
 
 // POSTS Route - GET
 app.get("/posts/:postID", async function(req, res) {
@@ -88,12 +88,7 @@ app.get("/posts/:postID", async function(req, res) {
      console.error(err);
      res.status(500).send("Error retrieving the post.");
    }
- });
-
-// Server Listener
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
 });
 
-app. use( '/.netlify/app.js' ,router) ;
-   module.exports.handter = serverless(app);
+// Serverless function setup for Vercel
+module.exports.handler = serverless(app);
